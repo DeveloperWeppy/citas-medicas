a8238c5689b120524855995912b7314dc0da82dd
DyamDev <dyamdev@gmail.com>
2025-11-19 15:10:30 -0500

    Boton de whatsapp

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -16,6 +16,27 @@
     <x-css>
         {{$css}}
     </x-css>
+    <style>
+        @media only screen and (max-width: 1199px) {
+            .whatsappFloat {
+                bottom: 30px !important;
+                right: 30px !important;
+            }
+        }
+        @media only screen and (max-width: 767px) {
+            .whatsappFloat {
+                bottom: 20px !important;
+                right: 20px !important;
+                height: 50px !important;
+                width: 50px !important;
+                line-height: 50px !important;
+            }
+        }
+        .whatsappFloat:hover {
+            color: #fff !important;
+            background: #128c7e !important;
+        }
+    </style>
 
 </head>
 <body>
@@ -25,8 +46,14 @@
 
         {{ $slot }}
 
+    <!-- |=====|| WhatsApp Float Button Start ||===============| -->
+    <a href="https://wa.me/573330333455?text=Hola%20necesito%20ayuda%20en%20citasmedicas.es" target="_blank" class="whatsappFloat" style="position: fixed; display: block; bottom: 50px; right: 35px; z-index: 100000; color: #fff; background: #25d366; height: 55px; width: 55px; text-align: center; line-height: 55px; -webkit-transition: 1s ease; -o-transition: 1s ease; transition: 1s ease; border-radius: 50%; font-size: 18px; -webkit-box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.14), 0 2px 8px 0 rgba(0, 0, 0, 0.1); box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.14), 0 2px 8px 0 rgba(0, 0, 0, 0.1); text-decoration: none;">
+        <i class="fab fa-whatsapp"></i>
+    </a>
+    <!-- |=====|| WhatsApp Float Button End ||=================| -->
+
     <!-- |=====|| ScrollToTop Start ||===============| -->
-    <a href="#" class="scrollToTop">
+    <a href="#" class="scrollToTop" style="display: inline;">
         <i class="fas fa-level-up-alt"></i>
     </a>
 
